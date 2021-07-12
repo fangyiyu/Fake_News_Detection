c745a76cb7d3f692386bb7427fece2c8f6a0ce57
Fangyi Yu <yufangyi1@gmail.com>
2021-07-12 12:50:59 -0400

    changed to my version

--- app.py
+++ app.py
@@ -1,82 +1,34 @@
-# from flask import Flask, render_template, request
-# import nltk
-# import pickle
-# from nltk.corpus import stopwords
-# import re
-# import os
-# from nltk.stem.porter import PorterStemmer
-
-# app = Flask(__name__)
-# ps = PorterStemmer()
-# nltk.data.path.append('./nltk_data')
-
-# model = pickle.load(open('model.pkl', 'rb'))
-# tfidfvect = pickle.load(open('tfidfvect.pkl', 'rb'))
-
-# @app.route('/', methods=['GET'])
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/', methods=['POST'])
-# def predict():
-#     original_text = request.form['text']
-#     review = re.sub('[^a-zA-Z]', ' ', original_text)
-#     review = review.lower()
-#     review = review.split()
-#     review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
-#     review = ' '.join(review)
-#     review_vect = tfidfvect.transform([review]).toarray()
-#     prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
-#     return render_template('index.html', text=original_text, result=prediction)
-
-
-# if __name__ == "__main__":
-#     app.run(port=3000, debug=True)
-
-from flask import Flask, jsonify, request, render_template, redirect, url_for
-from prediction_model import PredictionModel
-import pandas as pd
-from random import randrange
-from forms import OriginalTextForm
-
+from flask import Flask, render_template, request
+import nltk
+import pickle
+from nltk.corpus import stopwords
+import re
+import os
+from nltk.stem.porter import PorterStemmer
 
 app = Flask(__name__)
+ps = PorterStemmer()
+nltk.data.path.append('./nltk_data')
 
-app.config['SECRET_KEY'] = '4c99e0361905b9f941f17729187afdb9'
+model = pickle.load(open('model.pkl', 'rb'))
+tfidfvect = pickle.load(open('tfidfvect.pkl', 'rb'))
 
-
-@app.route("/", methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def home():
-    form = OriginalTextForm()
+    return render_template('index.html')
 
-    if form.generate.data:
-        data = pd.read_csv("random_dataset.csv")
-        index = randrange(0, len(data)-1, 1)
-        original_text = data.loc[index].text
-        form.original_text.data = str(original_text)
-        return render_template('home.html', form=form, output=False)
-
-    elif form.predict.data:
-        if len(str(form.original_text.data)) > 10:
-            model = PredictionModel(form.original_text.data)
-            return render_template('home.html', form=form, output=model.predict())
-
-    return render_template('home.html', form=form, output=False)
+@app.route('/', methods=['POST'])
+def predict():
+    original_text = request.form['text']
+    review = re.sub('[^a-zA-Z]', ' ', original_text)
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    review_vect = tfidfvect.transform([review]).toarray()
+    prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
+    return render_template('index.html', text=original_text, result=prediction)
 
 
-@app.route('/predict/<original_text>', methods=['POST', 'GET'])
-def predict(original_text):
-    #text = 'CAIRO (Reuters) - Three police officers were killed and eight others injured in a shoot-out during a raid on a suspected militant hideout in Giza, southwest of the Egyptian capital, two security sources said on Friday. The sources said authorities were following a lead to an apartment thought to house eight suspected members of Hasm, a group which has claimed several attacks around the capital targeting judges and policemen since last year. The suspected militants fled after the exchange of fire there, the sources said. Egypt accuses Hasm of being a militant wing of the Muslim Brotherhood, an Islamist group it outlawed in 2013. The Muslim Brotherhood denies this. An Islamist insurgency in the Sinai peninsula has grown since the military overthrew President Mohamed Mursi of the Muslim Brotherhood in mid-2013 following mass protests against his rule. The militant group staging the insurgency pledged allegiance to Islamic State in 2014. It is blamed for the killing of hundreds of soldiers and policemen and has started to target other areas, including Egypt s Christian Copts. ' 
-    model = PredictionModel(original_text)
-    return jsonify(model.predict())
-
-
-@app.route('/random', methods=['GET'])
-def random():
-    data = pd.read_csv("random_dataset.csv")
-    index = randrange(0, len(data)-1, 1)
-    return jsonify({'title': data.loc[index].title, 'text': data.loc[index].text, 'label': str(data.loc[index].label)})
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
