a732a3cb787a4befff579979da858925dbb75fae
Fangyi Yu <yufangyi1@gmail.com>
2021-08-07 10:58:41 -0400

    add new branch

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import nltk
 import pickle
 from nltk.corpus import stopwords
 import re
-import os
 from nltk.stem.porter import PorterStemmer
 
 app = Flask(__name__)
 ps = PorterStemmer()
-nltk.data.path.append('./nltk_data')
 
 model = pickle.load(open('model.pkl', 'rb'))
 tfidfvect = pickle.load(open('tfidfvect.pkl', 'rb'))
@@ -17,18 +15,29 @@ tfidfvect = pickle.load(open('tfidfvect.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def predict():
-    original_text = request.form['text']
-    review = re.sub('[^a-zA-Z]', ' ', original_text)
+def predict(text):
+    review = re.sub('[^a-zA-Z]', ' ', text)
     review = review.lower()
     review = review.split()
     review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
     review = ' '.join(review)
     review_vect = tfidfvect.transform([review]).toarray()
     prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
-    return render_template('index.html', text=original_text, result=prediction)
+    return prediction
+
+@app.route('/', methods=['POST'])
+def webapp():
+    text = request.form['text']
+    prediction = predict(text)
+    return render_template('index.html', text=text, result=prediction)
+
+
+@app.route('/predict/', methods=['GET','POST'])
+def api():
+    text = request.args.get("text")
+    prediction = predict(text)
+    return jsonify(prediction=prediction)
 
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(debug=True)
